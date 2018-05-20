b79ae08b2626ea8fbe8361f7de2c300c74b6050a
Chaouki Dhib <chaodhib@gmail.com>
2018-05-20 19:15:53 +0200

    Further improve first view: representation list. also add main navigation menu

--- src/main/java/com/chaouki/icc/reservations/controllers/RepresentationController.java
+++ src/main/java/com/chaouki/icc/reservations/controllers/RepresentationController.java
@@ -1,12 +1,15 @@
 package com.chaouki.icc.reservations.controllers;
 
+import com.chaouki.icc.reservations.entities.Location;
 import com.chaouki.icc.reservations.entities.Representation;
+import com.chaouki.icc.reservations.entities.Show;
 import com.chaouki.icc.reservations.services.RepresentationService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @ManagedBean
@@ -18,11 +21,45 @@ public class RepresentationController {
 
     private List<Representation> representations;
 
+    private LocalDateTime dateTime;
+    private Show show;
+    private Location location;
+
+    @PostConstruct
     public void loadList(){
         representations = representationService.findAll();
     }
 
+    public void create() {
+        Representation representation = new Representation();
+        representation.setWhen(dateTime);
+    }
+
     public List<Representation> getRepresentations() {
         return representations;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Show getShow() {
+        return show;
+    }
+
+    public void setShow(Show show) {
+        this.show = show;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
     }
 }
