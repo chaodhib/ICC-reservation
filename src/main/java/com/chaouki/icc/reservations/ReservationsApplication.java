6e8e21dbd90a3474df3d986af626316869814abe
Chaouki Dhib <chaodhib@gmail.com>
2018-06-14 21:27:31 +0200

    partial implementation of the authentifcation (non functional)

--- src/main/java/com/chaouki/icc/reservations/ReservationsApplication.java
+++ src/main/java/com/chaouki/icc/reservations/ReservationsApplication.java
@@ -2,9 +2,26 @@ package com.chaouki.icc.reservations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ReservationsApplication {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authProvider(UserDetailsService userDetailsService) {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+		daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+		return daoAuthenticationProvider;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationsApplication.class, args);
--- src/main/java/com/chaouki/icc/reservations/dao/UserDao.java
+++ src/main/java/com/chaouki/icc/reservations/dao/UserDao.java
@@ -3,6 +3,9 @@ package com.chaouki.icc.reservations.dao;
 import com.chaouki.icc.reservations.entities.Booking;
 import com.chaouki.icc.reservations.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UserDao extends JpaRepository<User, Integer> {
+
+    User findByLogin(String login);
 }
